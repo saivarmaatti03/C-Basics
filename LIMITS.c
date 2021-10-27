30aa612d21dc69389382322775ab804822e00c6a
Saivarma Atti <92287589+saivarmaatti03@users.noreply.github.com>
2021-10-27 10:33:01 +0530

    Add files via upload

---
+++ LIMITS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+void main()
+{
+	printf("Short int:\nSigned: %hd to %hd\n", SHRT_MIN, SHRT_MAX);
+	printf("Unsigned: %d to %hu\n", 0, USHRT_MAX);
+	printf("Int:\nSigned: %d to %d\n", INT_MIN, INT_MAX);
+	printf("Unsigned: %d to %u\n", 0, UINT_MAX);
+	printf("Long int:\nSigned: %ld to %ld\n", LONG_MIN, LONG_MAX);
+	printf("Unsigned: %d to %lu\n", 0, ULONG_MAX);
+	printf("Long long int:\nSigned: %lld to %lld\n", LONG_LONG_MIN, LONG_LONG_MAX);
+	printf("Unsigned: %d to %llu\n", 0, ULONG_LONG_MAX);
+}
