41258b907eb5451f76dbf34f169be0c43aad86a2
Saivarma Atti <92287589+saivarmaatti03@users.noreply.github.com>
2021-11-01 10:48:16 +0530

    Add files via upload

---
+++ exercise1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+ int a,b,c,d,e,f,g;
+ a=10;
+ b=2;
+ c=a+b;
+ d=a-b;
+ e=a*b;
+ f=a/b;
+ g=c+d+e+f;
+  printf("sum of %d and %d is %d\n",a,b,c);
+  printf("diference of %d and %d is %d\n",a,b,d);
+  printf("product of %d and %d is %d\n",a,b,e);
+  printf("quotient of %d and %d is %d\n",a,b,f);
+  printf("remainder of %d and %d is %d\n",a,b,g);
+  printf("sum of all arthemetic operations is %d",g);
+  }
+  
+  
